9d0558ac903ef9305ee19052d2a3ac0387980735
JarryLi <3244188372@qq.com>
2019-07-15 22:14:40 +0800

    [jerryLi] refactor: extra variable of Story2Test

--- src/test/java/com/thoughtworks/tdd/ParkingStoryTwoTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingStoryTwoTest.java
@@ -1,19 +1,29 @@
 package com.thoughtworks.tdd;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingStoryTwoTest {
+
+    private List<ParkingLot> parkingLots = new ArrayList<>();
+    private List<Car> cars = new ArrayList<>();
+    private BasicParkingBoy basicParkingBoy;
+
+    @BeforeEach
+    public void setUp(){
+        parkingLots.add(new ParkingLot(10));
+        cars.add(new Car());
+        cars.add(new Car());
+        basicParkingBoy = new BasicParkingBoy(parkingLots);
+    }
+
     @Test
     public void shout_return_error_message_when_call_fetch_given_usedTicket_or_wrongTicket(){
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(new ParkingLot(10));
-        BasicParkingBoy basicParkingBoy = new BasicParkingBoy(parkingLots);
-        Car car = new Car();
-        Ticket ticket = basicParkingBoy.park(car);
+        Ticket ticket = basicParkingBoy.park(cars.get(0));
         basicParkingBoy.fetch(ticket);
         Car fetch1 = basicParkingBoy.fetch(ticket);
         Assertions.assertEquals("Unrecognized parking ticket.", basicParkingBoy.getErrorMessage());
@@ -26,9 +36,6 @@ public class ParkingStoryTwoTest {
 
     @Test
     public void shout_return_error_message_when_call_fetch_given_nullTicket(){
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(new ParkingLot(10));
-        BasicParkingBoy basicParkingBoy = new BasicParkingBoy(parkingLots);
         basicParkingBoy.fetch(null);
 
         Assertions.assertEquals("Please provide your parking ticket.", basicParkingBoy.getErrorMessage());
@@ -36,9 +43,6 @@ public class ParkingStoryTwoTest {
 
     @Test
     public void shout_return_error_message_when_call_fetch_given_hasTicket_whit_limited_Lot(){
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(new ParkingLot(10));
-        BasicParkingBoy basicParkingBoy = new BasicParkingBoy(parkingLots);
         for (int i = 0; i < 10; i++) basicParkingBoy.park(new Car());
         basicParkingBoy.park(new Car());
 
