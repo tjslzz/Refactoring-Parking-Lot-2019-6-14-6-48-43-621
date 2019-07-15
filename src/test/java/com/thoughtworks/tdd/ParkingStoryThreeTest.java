27dd06fc3786fe5cc1561914b19111c499b9172f
JarryLi <3244188372@qq.com>
2019-07-15 22:15:41 +0800

    [jerryLi] refactor: extra variable of Story1Test

--- src/test/java/com/thoughtworks/tdd/ParkingStoryThreeTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingStoryThreeTest.java
@@ -1,17 +1,29 @@
 package com.thoughtworks.tdd;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingStoryThreeTest {
+
+    private List<ParkingLot> parkingLots = new ArrayList<>();
+    private List<Car> cars = new ArrayList<>();
+    private BasicParkingBoy basicParkingBoy;
+
+    @BeforeEach
+    public void setUp(){
+        parkingLots.add(new ParkingLot(10));
+        parkingLots.add(new ParkingLot(10));
+        cars.add(new Car());
+        cars.add(new Car());
+        basicParkingBoy = new BasicParkingBoy(parkingLots);
+    }
+
     @Test
     public void shout_return_car_when_call_fetch_given_hasTicket_with_number_11_Cars(){
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(new ParkingLot(10));parkingLots.add(new ParkingLot(10));
-        BasicParkingBoy basicParkingBoy = new BasicParkingBoy(parkingLots);
         for (int i = 0; i < 10; i++) basicParkingBoy.park(new Car());
         Car car = new Car();
         Ticket ticket = basicParkingBoy.park(car);
