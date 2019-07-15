3d18c5917f7a660da0bf4377ed05e0365e8c1068
JarryLi <3244188372@qq.com>
2019-07-15 22:17:18 +0800

    [jerryLi] refactor: extra variable of Story5Test

--- src/test/java/com/thoughtworks/tdd/ParkingStoryFiveTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingStoryFiveTest.java
@@ -1,17 +1,28 @@
 package com.thoughtworks.tdd;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingStoryFiveTest {
+    private List<ParkingLot> parkingLots = new ArrayList<>();
+    private List<Car> cars = new ArrayList<>();
+    private SuperSmartParkingBoy superSmartParkingBoy;
+
+    @BeforeEach
+    public void setUp(){
+        parkingLots.add(new ParkingLot(20));
+        parkingLots.add(new ParkingLot(10));
+        cars.add(new Car());
+        cars.add(new Car());
+        superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+    }
+
     @Test
     public void shout_return_car_when_call_fetch_given_hasTicket_with_superSmartBoy(){
-        List<ParkingLot> parkingLots = new ArrayList<>();
-        parkingLots.add(new ParkingLot(20));parkingLots.add(new ParkingLot(10));
-        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
         for (int i = 0; i < 15; i++) superSmartParkingBoy.park(new Car());
         Car car = new Car();
         Ticket ticket = superSmartParkingBoy.park(car);
